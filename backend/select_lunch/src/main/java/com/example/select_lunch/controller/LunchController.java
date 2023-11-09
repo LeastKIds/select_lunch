41f7168b5a4ece3ac64b3a9a7d88e7d3c32198e8
leastkids <leastkids@gmail.com>
2023-11-09 05:36:21 +0000

    ADD - google map place api

--- backend/select_lunch/src/main/java/com/example/select_lunch/controller/LunchController.java
+++ backend/select_lunch/src/main/java/com/example/select_lunch/controller/LunchController.java
@@ -22,26 +22,48 @@ public class LunchController {
     
     @GetMapping("/test")
     public String test() {
-        log.error("tttttttttttt");
         return "test  OKasdfafd";
     }
 
 
-    @GetMapping("/search/{search}")
-    public String search(@PathVariable String search) {
+    @GetMapping("/search/{keyward}")
+    public String search(@PathVariable String keyward) {
 
-        log.error("test");
+        // String baseUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json";
+        // String uri = UriComponentsBuilder.fromHttpUrl(baseUrl)
+        //         .queryParam("input", search)
+        //         .queryParam("inputtype", "textquery")
+        //         .queryParam("fields", "formatted_address,name,rating,opening_hours,geometry")
+        //         .queryParam("language", "ko")
+        //         .queryParam("key", env.getProperty("google.places.api_key"))
+        //         .encode()
+        //         .toUriString();
 
-        String baseUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json";
-        String uri = UriComponentsBuilder.fromHttpUrl(baseUrl)
-                .queryParam("input", search)
-                .queryParam("inputtype", "textquery")
-                .queryParam("fields", "formatted_address,name,rating,opening_hours,geometry")
-                .queryParam("key", env.getProperty("google.places.api_key"))
-                .encode()
-                .toUriString();
+        // return restTemplate.getForObject(uri, String.class);
 
-        return restTemplate.getForObject(uri, String.class);
+        // 35.622995778484864, 139.56164735954468 기숙사
+
+
+//         https://maps.googleapis.com/maps/api/place/photo
+//   ?maxheight=400
+//   &photoreference=[PHOTO_REFERENCE]
+//   &key=YOUR_API_KEY
+
+        String baseUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
+        String url = UriComponentsBuilder.fromHttpUrl(baseUrl)
+                    .queryParam("keyword", "Korean food")
+                    .queryParam("location", "35.622995778484864" + "," + "139.56164735954468")
+                    .queryParam("radius", 1000) // 단위는 미터
+                    // .queryParam("rankby", "prominence")
+                    .queryParam("type", "restaurant")
+                    .queryParam("language", "ko") // 검색 결과를 한국어로 받기 위해 추가
+                    .queryParam("key", env.getProperty("google.places.api_key"))
+                    
+
+                    .encode()
+                    .toUriString();
+
+        return restTemplate.getForObject(url, String.class);
 
     }
 }
