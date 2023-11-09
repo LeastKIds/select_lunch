a74a7feef8eca7e0113752324ad5efbf117bedc7
leastkids <leastkids@gmail.com>
2023-11-09 02:39:00 +0000

    ADD - google map

--- backend/select_lunch/src/main/java/com/example/select_lunch/controller/LunchController.java
+++ backend/select_lunch/src/main/java/com/example/select_lunch/controller/LunchController.java
@@ -1,15 +1,47 @@
 package com.example.select_lunch.controller;
 
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/")
+@Slf4j
 public class LunchController {
+
+    private final Environment env;
+    private final RestTemplate restTemplate;
     
     @GetMapping("/test")
     public String test() {
-        return "test  OK";
+        log.error("tttttttttttt");
+        return "test  OKasdfafd";
+    }
+
+
+    @GetMapping("/search/{search}")
+    public String search(@PathVariable String search) {
+
+        log.error("test");
+
+        String baseUrl = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json";
+        String uri = UriComponentsBuilder.fromHttpUrl(baseUrl)
+                .queryParam("input", search)
+                .queryParam("inputtype", "textquery")
+                .queryParam("fields", "formatted_address,name,rating,opening_hours,geometry")
+                .queryParam("key", env.getProperty("google.places.api_key"))
+                .encode()
+                .toUriString();
+
+        return restTemplate.getForObject(uri, String.class);
+
     }
 }
